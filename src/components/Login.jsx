a93c36cacceab9c9be0670100f3f4dc9deb05626
alber <albertinh623@gmail.com>
2021-05-23 17:47:32 +0200

    commit 23/05/2021

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
 //css
-import '../assets/css/Login.css'
+import '../assets/css/Login.css';
+
+import logo from '../assets/img/small.png';
+//servicio
+import {Apiurl} from '../service/apirest';
+//librerias
+import axios from 'axios';
+
 
 class Login extends React.Component{
+
+    state = {
+        form:{
+            "usuario":"",
+            "password":""
+        },
+        error:false,
+        errorMsg:""
+    }
+
+    managerSubmit = e =>{
+        e.preventDefault();
+    }
+
+    managerChange = async e=>{
+        await this.setState({
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }        
+        })
+    }
+
+    managerButton = () => {
+        let url = Apiurl + "login";
+        axios.post(url,this.state.form)
+        .then(response =>{
+            console.log(response);
+        })
+    }
 
     render(){
         return(
@@ -10,13 +47,13 @@ class Login extends React.Component{
                 <div className="wrapper fadeInDown">
                     <div id="formContent">
                         <div className="fadeIn first">
-                        <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+                        <img src={logo}id="icon" width="100px" alt="User Icon" />
                         </div>
 
-                        <form>
-                        <input type="text" id="login" class="fadeIn second" className="login" placeholder="login"/>
-                        <input type="text" id="password" class="fadeIn third" className="login" placeholder="password"/>
-                        <input type="submit" className="fadeIn fourth" value="Log In"/>
+                        <form onSubmit={this.managerSubmit}>
+                        <input type="text" className="fadeIn second" name="usuario" placeholder="usuario" onChange={this.managerChange}/>
+                        <input type="password" className="fadeIn third" name="password" placeholder="password" onChange={this.managerChange}/>
+                        <input type="submit" className="fadeIn fourth" value="Log In" onClick={this.managerButton}/>
                         </form>
 
                         <div id="formFooter">
